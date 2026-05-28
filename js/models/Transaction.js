f87d18f5ca4c2eae376194ee94411dcf84a3b99a
Kieu My <msi2310alice@gmail.com>
2026-05-28 13:57:17 +0700

    v2

--- js/models/Transaction.js
+++ js/models/Transaction.js
@@ -2,14 +2,12 @@ class Transaction{
     constructor(
         id, 
         transactionType, 
-        productId, 
         lotId, 
         transactionQuantity, 
         transactionDate
     ){
         this.id = id; 
         this.transactionType = transactionType;
-        this.productId = productId;
         this.lotId = lotId;
         this.transactionQuantity = transactionQuantity;
         this.transactionDate = transactionDate;
